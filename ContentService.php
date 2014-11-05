83d80501d441a9c0162fb4f45efe72c3aa2284c7
tjolliet <thomas.jolliet@francetv.fr>
2014-11-05 09:49:32 +0100

    correction refacto

--- ContentService.php
+++ ContentService.php
@@ -13,11 +13,22 @@ class ContentService extends Service
   public function getContentByTags($tags, $synonyms = false, $children = false, $page = 1, $limit = 100) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $this->urlAPI . "/contents/?tags=" . implode($tags, ',') . "&synonyms=$synonyms&children=$children&page=$page&limit=$limit");
+
+    $params = array(
+      'tags' => implode($tags, ','),
+      'synonyms' => $synonyms,
+      'children' => $children,
+      'page' => $page,
+      'limit' => $limit
+    );
+    curl_setopt($ch, CURLOPT_URL, $this->urlAPI . "/contents/" . parent::addParamUrl($params));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($ch, CURLOPT_HEADER, FALSE);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', "X-FTVEN-ID: " . $this->accessToken));
     $response = curl_exec($ch);
     curl_close($ch);
+
+    // TODO gestion des retours http
 
     $json = json_decode($response);
     $contents = array();
