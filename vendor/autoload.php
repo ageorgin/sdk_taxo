d0b7bd05b78691ae0088585fe09b8f08f83f10cf
Arnaud Georgin <arnaud.georgin.ext@francetv.fr>
2014-12-05 11:02:16 +0100

    Fix composer.lock #Sensiolabsinsight

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit48d06d51eb7c5f342f66d3a1d031a647::getLoader();
+return ComposerAutoloaderInit650a003237ad193e9a271413b1c45261::getLoader();
